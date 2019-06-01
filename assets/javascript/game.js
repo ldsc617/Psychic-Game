1b703cded1a5ea7852ea9a01a23c95acb8fc105c
Laura Salomon <laurasalomon@Lauras-MacBook-Pro.local>
2019-06-01 05:41:01 -0700

    its viible now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,52 @@
 //variables
+
 var  letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
-var guessesLeft = 10;
-var SoFarGuessed = []
+var guessesLeft = 9;
+var SoFarGuessed = [];
 
 
 
-var userGuess = document.onkeyup;
-var compGuess = letters[Math.floor(Math.random() * letters.length)];
 
-
-randomLetter = letters[Math.floor(Math.random() * letters.length)];
-console.log(randomLetter);
-
-function computerGuess() {
-    randomLetter = letters[Math.floor(Math.random() * letters.length)];
-	console.log(randomLetter);
+var reset = function () {
+  wins = 0;
+  losses = 0;
+  guessesLeft = 9;
+  SoFarGuessed = [];
 }
 
-
-
-document.onkeyup = function (event) {
+document.onkeypress = function (event) {
   
-  userGuess = event.key;
+  var userGuess = event.key;
+  var compGuess = letters[Math.floor(Math.random() * letters.length)];
   SoFarGuessed.push(userGuess);
+  console.log(userGuess);
 
-  if (compGuess == userGuess) {
+
+  if (compGuess === userGuess) {
     wins++;
-    guessesLeft = 10;
+    guessesLeft = 9;
     SoFarGuessed = [];
+    document.getElementById("wins").textContent = "wins:" + wins;
+    reset();
   }
   
-  if (compGuess != userGuess) {
+  else if (compGuess != userGuess) {
     guessesLeft--;
+    document.getElementById("so-far").textContent = SoFarGuessed;
     
+
   }
 
-  if (guessesLeft == 0) {
+ else if (guessesLeft === 0) {
     losses++;
-    guessesLeft = 10;
+    guessesLeft = 9;
     SoFarGuessed = [];
+    document.getElementById("losses").textContent = "losses:" + losses;
+    reset();
   }
+  console.log(guessesLeft);
 
  
-document.getElementById("game").innerHTML = html;
 }
